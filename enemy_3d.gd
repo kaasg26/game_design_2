64912af42ad4ea8801cdc7f2eb8d398e6e4a06cf
kaasg26 <162478377+kaasg26@users.noreply.github.com>
2025-02-11 09:37:52 -0600

    2/11

--- enemy_3d.gd
+++ enemy_3d.gd
@@ -4,10 +4,14 @@ extends CharacterBody3D
 @onready var atk_area = $AttackArea
 @onready var nav_agent = $NavigationAgent3D
 
+@onready var model = $Yellow_Biped
+@onready var animator = $Yellow_Biped/AnimationPlayer
+
 var SPEED = 3.0
 var ACCEL = 20
 var ATTACK = 10
 var KNOCKBACK = 16.0
+var WALK_SPEED = SPEED
 
 func take_damage(_dmg):
 	self.queue_free()
@@ -22,6 +26,10 @@ func _physics_process(delta: float) -> void:
 											.normalized() * KNOCKBACK
 	var dir = (nav_agent.target_position-self.global_position).normalized()
 	velocity = velocity.lerp(dir * SPEED, ACCEL * delta)
+	
+	
+	
+	animator.play("Robot_Soldat")
 	
 	
 	move_and_slide()
