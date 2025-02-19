c9aacd957c6b14b0c61ca449a6b913805b45cbf0
kaasg26 <162478377+kaasg26@users.noreply.github.com>
2025-02-19 09:34:43 -0600

    2/19

--- enemy_3d.gd
+++ enemy_3d.gd
@@ -12,6 +12,7 @@ var ACCEL = 20
 var ATTACK = 10
 var KNOCKBACK = 16.0
 var WALK_SPEED = SPEED
+var RUN_SPEED = SPEED * 1.5
 
 func take_damage(_dmg):
 	self.queue_free()
@@ -20,12 +21,19 @@ func _physics_process(delta: float) -> void:
 	for player in get_tree().get_nodes_in_group("Player"):
 		if $AttackRange.overlaps_body(player):
 			nav_agent.target_position = player.global_position
+			SPEED = RUN_SPEED
+		else:
+			WALK_SPEED
 		if atk_area.overlaps_body(player):
 			player.take_damage(ATTACK)
 			player.inertia = (player.global_position-self.global_position) \
 											.normalized() * KNOCKBACK
-	var dir = (nav_agent.target_position-self.global_position).normalized()
-	velocity = velocity.lerp(dir * SPEED, ACCEL * delta)
+	var dir = (nav_agent.target_position-self.global_position)
+	dir.y = 0
+	if dir.length() > 0.01:
+		var rot_angle = atan2(dir.x, dir.z)
+		self.rotation.y = lerp_angle(rotation.y, rot_angle, 5 * delta)
+	velocity = velocity.lerp(dir.normalized() * SPEED, ACCEL * delta)
 	
 	
 	
