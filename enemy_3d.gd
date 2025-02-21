7f4aee2f311d573fc1e7f1f1624ad13365bb7e34
kaasg26 <162478377+kaasg26@users.noreply.github.com>
2025-02-21 09:39:05 -0600

    2/21

--- enemy_3d.gd
+++ enemy_3d.gd
@@ -4,15 +4,15 @@ extends CharacterBody3D
 @onready var atk_area = $AttackArea
 @onready var nav_agent = $NavigationAgent3D
 
-@onready var model = $Yellow_Biped
-@onready var animator = $Yellow_Biped/AnimationPlayer
+@onready var model = $Poly_man_x
+@onready var animator = $Poly_man_x/AnimationPlayer
 
 var SPEED = 3.0
 var ACCEL = 20
 var ATTACK = 10
-var KNOCKBACK = 16.0
-var WALK_SPEED = SPEED
-var RUN_SPEED = SPEED * 1.5
+var KNOCKBACK = 8.0
+var WALKSPEED = SPEED
+var RUNSPEED = SPEED * 1.5
 
 func take_damage(_dmg):
 	self.queue_free()
@@ -21,9 +21,9 @@ func _physics_process(delta: float) -> void:
 	for player in get_tree().get_nodes_in_group("Player"):
 		if $AttackRange.overlaps_body(player):
 			nav_agent.target_position = player.global_position
-			SPEED = RUN_SPEED
+			SPEED = RUNSPEED
 		else:
-			WALK_SPEED
+			WALKSPEED
 		if atk_area.overlaps_body(player):
 			player.take_damage(ATTACK)
 			player.inertia = (player.global_position-self.global_position) \
@@ -36,9 +36,19 @@ func _physics_process(delta: float) -> void:
 	velocity = velocity.lerp(dir.normalized() * SPEED, ACCEL * delta)
 	
 	
+	if not is_on_floor():
+		velocity += get_gravity() * delta
 	
-	animator.play("Robot_Soldat")
+	if animator.current_animation != "ZombieAttack":
+		if velocity.length() <= 1:
+			animator.play("mixamo_com")
+		elif SPEED == WALKSPEED:
+			animator.play("Walking")
+		elif SPEED == RUNSPEED:
+			animator.play("Running")
 	
 	
 	move_and_slide()
 	
+func _ready():
+	nav_agent.target_position = global_position
