8f2b5a77eba2616912802da78bcd6292ea7ec2c1
kaasg26 <162478377+kaasg26@users.noreply.github.com>
2025-04-01 09:39:14 -0500

    4/1

--- fps_dart.gd
+++ fps_dart.gd
@@ -4,7 +4,7 @@ var ATTACK = 5
 var ATTACK_CRIT = 2 * ATTACK
 var SPEED = 50
 var DROP = 0.001
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity") * VisualShaderNodeColorOp
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity") * DROP
 var spawn_origin: Vector3
 
 #TODO AUDIO stuff
