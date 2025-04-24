7f3587fa49dbd42319a7814a7799d88fcb738c6a
kaasg26 <162478377+kaasg26@users.noreply.github.com>
2025-04-24 09:36:50 -0500

    4/24

--- game_design_2/random cutscenes/cutscene_3.gd
+++
@@ -1,8 +0,0 @@
-extends Node3D
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	await get_tree().create_timer(19.0).timeout
-	get_tree().quit()
-	pass 
